fa2404525e535de19ca04a279b4579dbd19c395b
Francislainy Campos <francislainy.campos@gmail.com>
2024-11-05 03:31:11 +0000

    Merge pull request #2
    * Configure test containers

---
+++ src/test/java/com/francislainy/sobe/config/BasePostgresConfig.java
@@ -0,0 +1,21 @@
+package com.francislainy.sobe.config;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public class BasePostgresConfig {
+
+    @Container
+    @ServiceConnection
+    public static PostgreSQLContainer<?> postgres;
+
+    static {
+        postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("latest"));
+        postgres.start();
+    }
+
+}
