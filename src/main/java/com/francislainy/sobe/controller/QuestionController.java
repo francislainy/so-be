c6cbad11e43360d1c73c6403f8a1ad191565a693
Francislainy Campos <francislainy.campos@gmail.com>
2024-11-08 16:02:21 +0000

    Fix compilation error by removing unused javax validation

--- src/main/java/com/francislainy/sobe/controller/QuestionController.java
+++ src/main/java/com/francislainy/sobe/controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.francislainy.sobe.controller;
 
 import com.francislainy.sobe.model.Question;
 import com.francislainy.sobe.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/questions")
